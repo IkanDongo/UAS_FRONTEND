9fd62f4ff93b8d18c6af52aee83f03b70794ba4c
Hendry535230151 <hendry.535230151@stu.untar.ac.id>
2024-11-20 21:13:24 +0700

    Adjusment Laravel

--- ZALORA/UAS_FE/config/database.php
+++ ZALORA/UAS_FE/config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -112,6 +112,11 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+                'driver' => 'mongodb',
+                'dsn' => env('DB_URI', 'mongodb+srv://hendry535230151:1234@cluster0.e1ht5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+                'database' => 'myappdb',
+        ],
     ],
 
     /*
