6d631fc86ccc4818f309818e2bde810a9c6c4fb1
Chiichii <sachigranita26@gmail.com>
2024-11-21 18:38:10 +0700

    ubah env

---
+++ ZALORA/UAS_FE/storage/framework/views/2fed6318a680db6b5bda7f80e66da60d.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\DATA1\kuliah\Frnt End\UAS_FRONTEND\ZALORA\UAS_FE\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
